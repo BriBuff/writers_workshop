5ec1840b7a43200b7a2f6fa5ae7a6c052c2a1b11
Jessica Katelyn Blomer <katelynblomer@gmail.com>
2019-03-24 16:56:17 -0600

    sundays database

--- server.js
+++ server.js
@@ -16,46 +16,41 @@ if (process.env.NODE_ENV === "production") {
 mongoose.connect("mongodb://localhost/writersworkshopdb", { useNewUrlParser: true });
 
 
-db.User.create({
-  name: "Jessica Blomer"},{password:"kate"})
+db.User.create(
+  {name: "bri", password:"katekate"})
   .then(function(dbUser){
-  console.log(dbUser)
+  console.log("user create" + dbUser)
 })
 .catch(function(err){
   console.log(err.message);
 });
 
 db.Project.create({
-title: "my first project",
-body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-last_updated: Date
+title: "bri project",
+body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even" 
 }).then(function(dbProject){
-  console.log(dbProject)
-}).catch(function(err){
-  console.log(err.message)
+
+  return db.User.update({}, { $push: { projects: dbProject._id } }, { new: true })
+}).then(function(dbUser){
+  console.log("user here" + dbUser)
+  
+})
+.catch(function(err){
+  console.log(err);
 });
 
-// Define API routes here
-// app.get("/dashboard", function(req, res){
-//   const name = req.params.name;
-//   db.User.find({
-    
-//   }).then(function(theUser){
-//     res.json(theUser)
-//   }).catch(function(err){
-//     console.log(err.message);
-//   })
-// });
+
 
 app.get("/dashboard/:name", function(req, res){
-  const name = req.params.name;
+  
   db.User.find({
-    name: name
-  }).populate("Project")
+
+  }).populate("projects")
   .then(function(theUser){
+    console.log(theUser)
     res.json(theUser)
   }).catch(function(err){
-    console.log(err.message);
+    res.json(err.message);
   })
 });
 // Send every other request to the React app
