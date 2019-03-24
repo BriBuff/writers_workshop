010306ee924c03ad7ea79f4d5f7f03bdec7bd42a
Jessica Katelyn Blomer <katelynblomer@gmail.com>
2019-03-24 13:58:42 -0600

    sunday

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,41 +1,5 @@
 import React from "react";
 
-// class Login extends React.Component {
-
-//   state={
-//     name: "",
-//     email: "",
-//     password: ""
-//   }
-
-//   render(){
-//     return(
-
-//       <section id="main">
-//       <div className="container">
-//           <div className="row">
-              
-//               <div className="col-md-4 col-md-offset-4">
-//                 <form id="login" action="index.html" className="well">
-//                   <div className="form-group">
-//                     <label>Email Address</label>
-//                     <input type="text" className="form-control" placeholder="Enter Email"></input>
-//                   </div>
-//                   <div className="form-group">
-//                       <label>Password</label>
-//                       <input type="password" className="form-control" placeholder="Password"></input>
-//                     </div>
-//                     <button type="submit" className="btn btn-default btn-block">Login</button>
-//                 </form>
-                            
-//               </div>
-//           </div>
-//       </div>
-//   </section>
-//   );
-//   }
-// }
-
 
 function Login(props){
     return(
